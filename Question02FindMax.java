3e9dcf6a9adfdf08e953a9e66eeabf5b8f9f59e4
Abhinav <abhinavravi.shankar.s.112@kalvium.community>
2026-02-05 12:05:00 +0530

    Add question 02

---
+++ Question02FindMax.java
@@ -0,0 +1,25 @@
+// Question: Find the maximum value in an integer array.
+// Solution: Track the current maximum while scanning the array once.
+
+public class Question02FindMax {
+    public static int findMax(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(findMax(arr));
+        scanner.close();
+    }
+}
