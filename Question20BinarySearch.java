0c967aeb658bc3829fd0187194e82fb7fa5da307
Abhinav <abhinavravi.shankar.s.112@kalvium.community>
2026-03-04 16:00:00 +0530

    Add Question20BinarySearch

---
+++ Question20BinarySearch.java
@@ -0,0 +1,32 @@
+// Question: Perform binary search in a sorted array.
+// Solution: Repeatedly divide search range in half.
+
+public class Question20BinarySearch {
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int target = scanner.nextInt();
+        System.out.println(binarySearch(arr, target));
+        scanner.close();
+    }
+}
